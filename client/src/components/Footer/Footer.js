f43d5f31f77b9e975d74fa5f979f1da027086db6
Mr. Akash Rathod <114861476+ak-akash03@users.noreply.github.com>
2024-03-04 23:54:16 +0530

    new update on footer componants

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -8,17 +8,6 @@ function Footer() {
 <div className="d-flex flex-column h-100">
 {/* 
 
-<section className="hero text-white py-5 flex-grow-1">
-    <div className="container py-4">
-        <div className="row">
-            <div className="col-lg-6">
-                <h1 className="display-4">Bootstrap footer bottom</h1>
-                <p className="fst-italic text-muted">Using Bootstrap 5 flexbox utilities, create a footer that always sticks to the bottom of your viewport. Snippet by <a className="text-primary" href="https://bootstrapious.com/" target="_blank">Bootstrapious</a></p>
-            </div>
-        </div>
-    </div>
-</section> */}
-
 
 
 <footer className="w-100 py-4 flex-shrink-0">
